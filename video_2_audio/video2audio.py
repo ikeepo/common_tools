ae944fb7a9729184b749b44218764a45c8256a5b
ikeepo <ikeepo@proton.me>
2024-11-05 10:26:48 +0800

    u

--- video_2_audio/video2audio.py
+++ video_2_audio/video2audio.py
@@ -2,7 +2,14 @@ import sys
 import os
 from pathlib import Path
 from moviepy.editor import *
+import yaml
 
+def load_from_yaml():
+    dp_pwd = Path(__file__).parent
+    yaml_file_path = dp_pwd / "config.yaml"
+    with open(yaml_file_path, "r") as file:
+        data = yaml.safe_load(file)
+    return data
 
 def v2a(fp_src, fp_tgt):
     video = VideoFileClip(fp_src)  # 2.
@@ -11,19 +18,21 @@ def v2a(fp_src, fp_tgt):
     print(f"{fp_src}->{fp_tgt}")
 
 
-def convert_dp(dp_video, dp_audio):
+def convert_dp(dp_video, dp_audio, remove_video=False):
     os.makedirs(dp_audio, exist_ok=True)
     for fn in os.listdir(dp_video):
         fp_video = f"{dp_video}/{fn}"
+        if os.path.isdir(fp_video):
+          continue
         fn_audio = f"{Path(fn).stem}.mp3"
         fp_audio = f"{dp_audio}/{fn_audio}"
         v2a(fp_video, fp_audio)
+        if remove_video:
+          os.system(f"rm {fp_video}")
 
 
 if __name__ == "__main__":
-    # fp_in = f"{dp_video}/06.mp4"
-    # fp_out = f"{dp_audio}/06.mp3"
-    # v2a(fp_in, fp_out)
-    dp_video = "/mnt/e/老王/video"
-    dp_audio = "/mnt/e/老王/audio"
-    convert_dp(dp_video, dp_audio)
+    data = load_from_yaml()
+    dp_video = data.get("dp_video", "")
+    dp_audio = data.get("dp_audio", "")
+    convert_dp(dp_video, dp_audio, remove_video=True)
