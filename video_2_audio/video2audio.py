39b33fd9522a79e527d98f763488effad1c9ed2d
ikeepo <ikeepo@proton.me>
2024-11-06 10:19:06 +0800

    u

--- video_2_audio/video2audio.py
+++ video_2_audio/video2audio.py
@@ -21,15 +21,19 @@ def v2a(fp_src, fp_tgt):
 def convert_dp(dp_video, dp_audio, remove_video=False):
     os.makedirs(dp_audio, exist_ok=True)
     for fn in os.listdir(dp_video):
-        fp_video = f"{dp_video}/{fn}"
+        fp_video = os.path.join(dp_video, fn)
         if os.path.isdir(fp_video):
+          continue
+        if fn.endswith("mp3") or fn.endswith("DS_Store"):
           continue
         fn_audio = f"{Path(fn).stem}.mp3"
         fp_audio = f"{dp_audio}/{fn_audio}"
-        v2a(fp_video, fp_audio)
+        if os.path.exists(fp_audio):
+           print(f"{fp_audio} already exist")
+        else:
+           v2a(fp_video, fp_audio)
         if remove_video:
-          os.system(f"rm {fp_video}")
-
+          os.remove(fp_video)
 
 if __name__ == "__main__":
     data = load_from_yaml()
